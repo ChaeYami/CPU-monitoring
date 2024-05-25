7c980bdb8449de69f18fab1fd57bad98fc7380e7
Chaeyeon Seo <ssallyseo@gmail.com>
2024-05-25 20:03:58 +0900

    first commit

---
+++ src/main/java/com/example/cpumonitoring/CpumonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cpumonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpumonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpumonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cpumonitoring/CpumonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpumonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpumonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
