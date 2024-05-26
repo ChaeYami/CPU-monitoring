541f6816e9e6c3ab3b3522da1e9d5bcc1ddf66af
Chaeyeon Seo <ssallyseo@gmail.com>
2024-05-27 04:08:26 +0900

    Feat: Add ExceptionHandler for invalid parameters in API requests.

---
+++ src/main/java/com/example/cpumonitoring/config/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.cpumonitoring.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid parameter: " + ex.getName());
+    }
+}
